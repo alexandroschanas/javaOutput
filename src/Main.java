aa3eb95bb7efea7120f9a72d54e8c517f6277cb4
Alexandros Chanas <alexnwris@Live.com>
2026-03-05 17:22:51 +0200

    Learning more about the strings and their methods

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
 
+        printInformation("kokokooko");
 
     }
+
+    public static void printInformation(String string){
+        int length = string.length();
+        System.out.printf("Length =%d %n", length);
+        System.out.printf("First char = %c %n", string.charAt(0));
+
+    }
+
 }
